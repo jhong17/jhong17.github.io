a6e6180768276668865dc0cda95a47a5367629fd
Jessica Hong <jessica@helloyada.com>
2020-04-22 00:27:28 -0400

    express/ejs

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const http = require('http');
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
 const port = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', function (req, res) {
+  res.render('index');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.listen(port, function () {
+  console.log(`Server running at port ${port}`);
 });
